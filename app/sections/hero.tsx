535681e0246c9a14df444126719f64461672906e
Priyanshu Raj <letscodez17@gmail.com>
2024-10-27 12:35:06 +0530

    Co-authored-by: letscodez17@gmail.com <letscodez17@gmail.com>

--- app/sections/hero.tsx
+++ app/sections/hero.tsx
@@ -3,63 +3,58 @@ import { heroImage, title } from "@/images/main";
 import React from "react";
 import Btn from "../components/btn";
 import Slide from "../components/slide";
-
 const HeroSection = () => {
   return (
-    <section className="w-screen pt-28 sm:pt-52 min-h-screen space-y-12 lg:space-y-0">
-      <div className="flex ml-2 md:pl-8 flex-col md:flex-row md:justify-center items-center text-white">
-        {/* Left side: Text content */}
-        <div className="md:w-1/2 mb-8 md:mb-0 mx-4 flex flex-col items-center md:items-start justify-center">
-          <Image
-            src={title}
-            alt="Hero Title"
-            height={90}
-            width={670}
-            className="sm:ml-0 ml-3 md:w-96 w-[320px]"
-            priority
-          />
+    <section className="w-screen bg-hero bg-no-repeat bg-cover bg-center bg-fixed min-h-screen px-2 pr-14 pt-32 md:pt-16 flex items-center md:pl-8 flex-col md:flex-row justify-center text-white">
+      {/* Left side: Text content */}
+      <div className="md:w-1/2 md:mb-0 mx-4 flex flex-col md:items-start md:justify-start items-center justify-center">
+        <Image
+          src={title}
+          alt="Hero Title"
+          height={90}
+          width={670}
+          className="sm:ml-0 ml-3 md:w-96 w-[320px]"
+          priority
+        />
 
-          <h1 className="text-[#06d6a0] 2xl:text-3xl text-2xl overlock-regular-italic my-4 text-center md:text-start">
-            "Glow Together, Grow Together"
-          </h1>
-          <h2 className="text-2xl font-semibold mb-4 text-white text-center md:text-start">
-            Empowering Your Academic journey with Eduvity!
-          </h2>
-          <p className="2xl:text-2xl text-lg text-gray-300 mb-8 text-center md:text-start">
-            Discover an all-in-one platform for curated modules,
-            textbooks, recommended books, and free courses to excel in your learning journey on each step.
+        <h1 className="text-[#06d6a0] 2xl:text-3xl text-2xl overlock-regular-italic my-4 text-center md:text-start">
+          "Know Together, Grow Together"
+        </h1>
+        <h2 className="text-2xl font-semibold mb-4 text-white text-center md:text-start">
+          Empowering Your Academic journey with Eduvity!
+        </h2>
+        <p className="2xl:text-2xl text-lg text-gray-300 mb-8 text-center md:text-start">
+          Discover an all-in-one platform for curated modules,
+          textbooks,recommended books, and free courses.
+        </p>
+
+        {/* LET'S GROW TOGETHER section */}
+        <div className="flex items-center justify-center md:justify-start mb-8 max-w-[480px] w-full">
+          <div className="flex-grow bg-[#06d6a0] h-[1px]" />
+          <p className="text-md 2xl:text-xl text-white font-semibold text-center mx-4 tracking-wider">
+            LET'S GROW TOGETHER
           </p>
-
-          {/* LET'S GROW TOGETHER section */}
-          <div className="flex items-center justify-center md:justify-start mb-8 max-w-[480px] w-full">
-            <div className="flex-grow bg-[#06d6a0] h-[1px]" />
-            <p className="text-md 2xl:text-xl text-white font-semibold text-center mx-4 tracking-wider">
-              LET'S GROW TOGETHER
-            </p>
-            <div className="flex-grow bg-[#06d6a0] h-[1px]" />
-          </div>
-
-          <div className="flex justify-center md:justify-start w-full">
-            <Btn />
-          </div>
+          <div className="flex-grow bg-[#06d6a0] h-[1px]" />
         </div>
 
-        {/* Right side: Image */}
-        <div className="md:w-1/2 flex justify-center md:justify-end">
-          <Image
-            src={heroImage}
-            alt="Hero Image"
-            className="md:rounded-l-3xl rounded-3xl shadow-2xl w-11/12"
-            width={600}
-            height={400}
-            priority
-          />
+        <div className="flex justify-center md:justify-start w-full mb-6">
+          <Btn />
         </div>
       </div>
-      <div className="lg:absolute bottom-0 h-[15vh] sm:right-8 sm:left-8 right-0 left-0">
-        {/* <Slide /> */}
+
+      {/* Right side: Image */}
+      <div className="md:w-1/2 flex justify-center md:justify-end">
+        <Image
+          src={heroImage}
+          alt="Hero Image"
+          className="md:rounded-l-3xl rounded-3xl shadow-2xl w-11/12"
+          width={600}
+          height={400}
+          priority
+        />
       </div>
     </section>
+    ///hnn bata abb kya krti ham
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,7 +38,10 @@ module.exports = {
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out'
-  		}
+  		},
+		  backgroundImage: {
+			'hero': "url('/images/heroImage.webp')",
+		},
   	}
   },
   plugins: [require("tailwindcss-animate")],
